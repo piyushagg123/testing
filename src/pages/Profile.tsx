7b9fe0e2b681a4ba1f698535a19cf97305135cd0
piyushagg123 <piyushaggarwal20903@gmail.com>
2024-08-09 09:03:26 +0530

    styling changes in profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,10 +2,10 @@ import { useContext, useState } from "react";
 import img from "../assets/background.jpg";
 import projectImage from "../assets/noProjectAdded.jpg";
 import reviewImage from "../assets/noReviewsAdded.png";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import {
   Box,
   Chip,
@@ -67,6 +67,7 @@ const Profile = () => {
   const [open, setOpen] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [selectedSubCategories, setSelectedSubCategories] = useState([]);
+  const [selectedProject, setSelectedProject] = useState<ProjectItem>();
   const navigate = useNavigate();
 
   const [projectId, setProjectId] = useState<number>(0);
@@ -84,6 +85,14 @@ const Profile = () => {
     setOpen(false);
     setIsSubmitted(false);
     setSelectedSubCategories([]);
+  };
+
+  const handleCarouselClick = (project: ProjectItem) => {
+    setSelectedProject(project);
+  };
+
+  const handleBackClick = () => {
+    setSelectedProject(undefined);
   };
 
   const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
@@ -206,46 +215,63 @@ const Profile = () => {
           </TabPanel>
 
           <TabPanel value={value} index={1}>
-            <div
-              className={`md:w-[500px] lg:w-[750px] flex justify-center flex-col items-center`}
-            >
+            <div className="md:w-[500px] lg:w-[750px] flex justify-center flex-col items-center">
               <br />
-              <div className="flex w-full justify-end">
-                <button
-                  className="flex items-center gap-2 p-2 border-text border-[2px] text-text bg-prim hover:bg-sec hover:border-text rounded-[5px]"
-                  onClick={() => setOpen(true)}
-                >
-                  <AddCircleIcon /> Add a new project
-                </button>
-              </div>
-              <br />
-              <div className="md:w-[500px] lg:w-[750px] flex">
+              <div className="flex flex-wrap">
                 {!projectsData ? (
-                  <div className="flex flex-col items-center justify-center md:w-[500px] lg:w-[750px]">
-                    <div className="">
+                  <div className="flex flex-col items-center justify-center">
+                    <div>
                       <img src={projectImage} alt="" className="w-[300px]" />
                     </div>
                     <br />
                     <p className="">No projects added yet by the designer</p>
                     <br />
                   </div>
+                ) : selectedProject ? (
+                  <div className="flex flex-col">
+                    <div className="flex justify-start gap-60 md:w-[500px] lg:w-[750px]">
+                      <button
+                        className="self-start mb-4 px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded"
+                        onClick={handleBackClick}
+                      >
+                        <ArrowBackIcon />
+                      </button>
+                    </div>
+                    <br />
+                    <div className="flex flex-col gap-3">
+                      <Carousel
+                        imageObj={selectedProject.images}
+                        showProjectDetails={false}
+                        city=""
+                        state=""
+                        theme=""
+                        title=""
+                      />
+                    </div>
+                    <br />
+                  </div>
                 ) : (
                   <div className="flex flex-wrap md:w-[500px] lg:w-[750px] justify-between">
-                    {projectsData.map((item: ProjectItem, ind: number) => (
-                      <Carousel
-                        imageObj={item.images}
-                        title={item.title}
-                        city={item.city}
-                        state={item.state}
-                        theme={item.sub_category_1}
+                    {projectsData.map((item: any, ind: number) => (
+                      <div
                         key={ind}
-                        showProjectDetails={true}
-                      />
+                        onClick={() => handleCarouselClick(item)}
+                        className="mb-4"
+                      >
+                        <Carousel
+                          key={ind}
+                          imageObj={item.images}
+                          title={item.title}
+                          city={item.city}
+                          state={item.state}
+                          theme={item.sub_category_1}
+                          showProjectDetails={true}
+                        />
+                      </div>
                     ))}
                   </div>
                 )}
               </div>
-              <br />
               <br />
               <br />
             </div>
@@ -268,102 +294,169 @@ const Profile = () => {
         </div>
       </div>
       <br />
-      <div className="w-[200px] text-lg ml-10">
+      <div className="w-[250px] text-lg ml-10">
         <br />
+        <br />
+        <div className=" ">
+          <p className="font-bold text-base text-darkgrey">Contact Number</p>
+          <p className="text-[16px]">{data?.mobile ?? "N/A"}</p>
+        </div>
+        <br />
+        <div className=" ">
+          <p className="font-bold text-base text-darkgrey">Email</p>
+          <p className="text-[16px]">{data?.email ?? "N/A"}</p>
+        </div>
         <br />
         <div className="flex flex-col justify-evenly gap-6">
-          <div className=" ">
-            <p className="font-bold text-base text-darkgrey">Business Name</p>
-            <p className="text-[16px]">{data.business_name}</p>
-          </div>
-          <div className=" ">
-            <p className="font-bold text-base text-darkgrey">
-              Typical Job Cost
-            </p>
-            <p className="text-[16px]">{data.average_project_value}</p>
-          </div>
-          <div className=" ">
-            <p className="font-bold text-base text-darkgrey">
-              Number of employees
-            </p>
-            <p className="text-[16px]">{data.number_of_employees}</p>
-          </div>
-          <div className=" ">
-            <p className="font-bold text-base text-darkgrey">
-              Projects Completed
-            </p>
-            <p className="text-[16px]">{data.projects_completed}</p>
-          </div>
-
-          <div className=" ">
-            <p className="font-bold text-base text-darkgrey">Contact Number</p>
-            <p className="text-[16px]">{data.mobile}</p>
-          </div>
-          {data.social ? (
+          {selectedProject ? (
             <>
-              {data.social.instagram || data.social.facebook ? (
+              <div>
+                <p className="font-bold text-base text-purple">
+                  Project details
+                </p>
+                <p className="font-bold text-base text-darkgrey">Title</p>
+                <p className="text-[16px] max-w-[300px]">
+                  {selectedProject.title}
+                </p>
+              </div>
+              <div>
+                <p className="font-bold text-base text-darkgrey">Description</p>
+                <p className="text-[16px] max-w-[300px]">
+                  {selectedProject.description}
+                </p>
+              </div>
+              <div>
+                <p className="font-bold text-base text-darkgrey">City</p>
+                <p className="text-[16px] max-w-[300px]">
+                  {selectedProject.city}
+                </p>
+              </div>
+              <div>
+                <p className="font-bold text-base text-darkgrey">State</p>
+                <p className="text-[16px] max-w-[300px]">
+                  {selectedProject.state}
+                </p>
+              </div>
+              <div>
+                <p className="font-bold text-base text-darkgrey">Spaces</p>
+                <p className="text-[16px]">
+                  {formatCategory(selectedProject.sub_category_2)
+                    .split(",")
+                    .map((item, ind) => (
+                      <>
+                        <Chip
+                          label={item}
+                          variant="outlined"
+                          key={ind}
+                          sx={{ height: "25px" }}
+                          style={{
+                            color: "linear-gradient(#ff5757,#8c52ff)",
+                          }}
+                        />
+                      </>
+                    ))}
+                </p>
+              </div>
+              <div>
+                <p className="font-bold text-base text-darkgrey">Theme</p>
+                <p className="text-[16px]">
+                  {formatCategory(selectedProject.sub_category_1)
+                    .split(",")
+                    .map((item, ind) => (
+                      <>
+                        <Chip
+                          label={item}
+                          variant="outlined"
+                          key={ind}
+                          sx={{ height: "25px" }}
+                          style={{
+                            color: "linear-gradient(#ff5757,#8c52ff)",
+                          }}
+                        />
+                      </>
+                    ))}
+                </p>
+              </div>
+              <div>
+                <p className="font-bold text-base text-darkgrey">Start Date</p>
+                <p className="text-[16px] max-w-[300px]">
+                  {selectedProject.start_date}
+                </p>
+              </div>
+              <div>
+                <p className="font-bold text-base text-darkgrey">End date</p>
+                <p className="text-[16px] max-w-[300px]">
+                  {selectedProject.end_date}
+                </p>
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                <p className="font-bold text-base text-darkgrey">
+                  Typical Job Cost
+                </p>
+                <p className="text-[16px]">
+                  {data?.average_project_value ?? "N/A"}
+                </p>
+              </div>
+              <div className=" ">
+                <p className="font-bold text-base text-darkgrey">
+                  Number of employees
+                </p>
+                <p className="text-[16px]">
+                  {data?.number_of_employees ?? "N/A"}
+                </p>
+              </div>
+              <div className=" ">
+                <p className="font-bold text-base text-darkgrey">
+                  Projects Completed
+                </p>
+                <p className="text-[16px]">
+                  {data?.projects_completed ?? "N/A"}
+                </p>
+              </div>
+
+              <div className=" ">
+                <p className="font-bold text-base text-darkgrey">Location</p>
+                <p className="text-[16px]">{data?.city ?? "N/A"}</p>
+              </div>
+              {data?.social ? (
                 <>
-                  <div className=" ">
+                  <div>
                     <p className="font-bold text-base text-darkgrey">Socials</p>
-                    <div className="flex gap-3 text-[16px]">
-                      <a href={data?.social?.facebook}>
+                    {data.social.facebook && (
+                      <a
+                        href={data.social.facebook}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
                         <FacebookIcon />
                       </a>
-                      <a href={data?.social?.instagram}>
+                    )}
+                    {data.social.instagram && (
+                      <a
+                        href={data.social.instagram}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
                         <InstagramIcon />
                       </a>
-                    </div>
+                    )}
+                    {data.social.website && (
+                      <a
+                        href={data.social.website}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <OpenInNewIcon />
+                      </a>
+                    )}
                   </div>
                 </>
-              ) : (
-                <></>
-              )}
+              ) : null}
             </>
-          ) : (
-            <></>
           )}
-
-          <div className=" ">
-            <p className="font-bold text-base text-darkgrey">Address</p>
-            <p className="text-[16px]">
-              {data.address} <br />
-              {data.city} <br />
-              {data.state}
-            </p>
-          </div>
-          {data.social ? (
-            <>
-              {data.social.website ? (
-                <>
-                  <div className=" ">
-                    <p className="font-bold text-base text-darkgrey">Website</p>
-                    <a
-                      href={data?.social?.website}
-                      className="flex items-center gap-1 text-[16px]"
-                    >
-                      homezdesigners.com <OpenInNewIcon />
-                    </a>
-                  </div>
-                </>
-              ) : (
-                <></>
-              )}
-            </>
-          ) : (
-            <></>
-          )}
-
-          <div className=" ">
-            <p className="font-bold text-base text-darkgrey">Email</p>
-            <a
-              href={`mailto:${data.email}`}
-              className="flex items-center gap-1 text-[16px]"
-            >
-              {data.email} <OpenInNewIcon />
-            </a>
-          </div>
-          <br />
-          <br />
         </div>
       </div>
 
