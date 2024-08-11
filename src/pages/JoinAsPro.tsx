8fc430202e9f97e256432190d0fe8857194cf687
piyushagg123 <piyushaggarwal20903@gmail.com>
2024-08-11 23:23:37 +0530

    join as pro error resolve

--- src/pages/JoinAsPro.tsx
+++ src/pages/JoinAsPro.tsx
@@ -162,9 +162,9 @@ const JoinAsPro: React.FC<JoinAsProProps> = ({ handleClose }) => {
         });
       }
     } catch (error) {}
+    navigate("/");
     window.location.reload();
     handleClose();
-    navigate("/");
   };
 
   const nextStep = () => {
