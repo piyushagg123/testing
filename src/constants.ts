e3572d0fb000d74ce4f163ae6910c2a6d47a24b0
piyushagg123 <piyushaggarwal20903@gmail.com>
2024-08-12 09:38:18 +0530

    api url updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 const constants = {
-    apiBaseUrl: "https://designmatch.ddns.net",
+    apiBaseUrl: "https://pickele-backend.ddns.net",
     apiImageUrl:"https://designmatch-s3-bucket.s3.ap-south-1.amazonaws.com",
     email:"communications@pickele.com"
   };
