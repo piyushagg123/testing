4b0ac36b023836638eecf3852b40a51f5dc17bb7
piyushagg123 <piyushaggarwal20903@gmail.com>
2024-12-01 21:34:24 +0530

    changes

--- src/pages/finance-planners/Types.ts
+++ src/pages/finance-planners/Types.ts
@@ -1,20 +1,20 @@
 export interface VendorData {
   logo?: string | null;
-  deals: string[] | string;
-  investment_ideology: string[] | string;
-  fees_type: string[] | string;
-  fees: number;
-  number_of_clients: number;
-  aum_handled: number;
+  deals?: string[] | string;
+  investment_ideology?: string[] | string;
+  fees_type?: string[] | string;
+  fees?: number;
+  number_of_clients?: number;
+  aum_handled?: number;
   sebi_registered?: boolean;
-  minimum_investment: number;
-  description: string;
-  business_name: string;
-  number_of_employees: number;
+  minimum_investment?: number;
+  description?: string;
+  business_name?: string;
+  number_of_employees?: number;
   mobile?: string;
   email?: string;
-  started_in: string;
-  city: string;
+  started_in?: string;
+  city?: string;
   state?: string;
   address?: string;
   social?: {
